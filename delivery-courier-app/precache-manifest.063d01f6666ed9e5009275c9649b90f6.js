5158811b19dbab1f8a7aee147cc41b4134327e00
Vitali Barushka <vitali@keywee.co>
2020-01-14 09:33:37 +0300

    delivery courier app

---
+++ delivery-courier-app/precache-manifest.063d01f6666ed9e5009275c9649b90f6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6004c9be96fb758b7909e76346e5e773",
+    "url": "./index.html"
+  },
+  {
+    "revision": "97d8afef3f57937cbdc5",
+    "url": "./static/css/2.7997c105.chunk.css"
+  },
+  {
+    "revision": "93944f203b6d18277d6a",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "97d8afef3f57937cbdc5",
+    "url": "./static/js/2.e340b3b6.chunk.js"
+  },
+  {
+    "revision": "3b4e29aebcd4cfff614eb6660450026c",
+    "url": "./static/js/2.e340b3b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "93944f203b6d18277d6a",
+    "url": "./static/js/main.809697cb.chunk.js"
+  },
+  {
+    "revision": "bc2d7413a490b7a863e2",
+    "url": "./static/js/runtime-main.241b80af.js"
+  },
+  {
+    "revision": "e04f2cdfc655aec4450d195aedac50d9",
+    "url": "./static/media/logo.e04f2cdf.svg"
+  },
+  {
+    "revision": "fb3b97e815266feb78d7d5d307ab10e2",
+    "url": "./static/media/mascot-login-page.fb3b97e8.png"
+  }
+]);
